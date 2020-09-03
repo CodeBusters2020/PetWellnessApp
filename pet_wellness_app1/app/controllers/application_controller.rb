0a538c4004b769c1a03f6545215bebf10b4472cd
LEARN Academy <learnacademy@LEARNs-MacBook-Air.local>
2020-09-03 16:43:41 -0700

    username is now accepted into database

--- pet_wellness_app1/app/controllers/application_controller.rb
+++ pet_wellness_app1/app/controllers/application_controller.rb
@@ -2,7 +2,16 @@ class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
 
   def after_sign_in_path_for(resource)
-  '/mypets'
+   '/mypets'
   end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+   devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  end
+
 
 end
